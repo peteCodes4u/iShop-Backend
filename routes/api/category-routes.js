98eac1438bc4029c481aa9bbf5b3162046da1c43
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-05-30 11:32:45 -0400

    configuration category routes wip

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,36 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ all: true, nested: true }],
+    });
+
+    res.status(200).json((categoryData));
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findOne(
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+    res.status(200).json((categoryData));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  
 });
 
 router.post('/', (req, res) => {
@@ -24,5 +46,28 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
 });
+
+router.post('/seed', (req, res) => {
+  Category.bulkCreate([
+    {
+      category_name: 'Shirts',
+    },
+    {
+      category_name: 'Shorts',
+    },
+    {
+      category_name: 'Music',
+    },
+    {
+      category_name: 'Hats',
+    },
+    {
+      category_name: 'Shoes',
+    },
+  ])
+  .then(()=> {
+    res.status(200).json({message: "Categories table seeded"})
+  })
+})
 
 module.exports = router;
