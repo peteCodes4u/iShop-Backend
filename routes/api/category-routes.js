5cb532d9e7f1f33b6cd76907b868ff8f07ccaaa1
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-05-30 16:16:46 -0400

    Compelted routes for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,8 +4,7 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', async (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+
   try {
     const categoryData = await Category.findAll({
       include: [{ all: true, nested: true }],
@@ -18,56 +17,63 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
   try {
     const categoryData = await Category.findOne(
-      {
-        where: {
-          id: req.params.id
-        }
-      }
+      {where: {id: req.params.id}, returning: true},
+      {include: [{ all: true, nested: true }]}, 
     )
+    if(!categoryData) {
+      return res.status(422).json({message: `an error has occurred, category with id ${req.params.id} is not found in our records`});
+    }
     res.status(200).json((categoryData));
   } catch (err) {
-    res.status(500).json(err);
+    res.status(422).json({message: "Sorry, your request could not be processed because of the follwoing error - " + err});
   }
   
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try{
+    await Category.create({
+      category_name: req.body.category_name 
+    });
+    res.status(200).json({message: `new category ${req.body.category_name} successfully created!`})
+  } catch (err) {
+    res.status(422).json({message: "Sorry, your request could not be processed because of the follwoing error - " + err});
+  };
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+ try{
+  const [rowsAffected, [updatedCategory]] = await Category.update(
+  {category_name: req.body.category_name },
+  {where: {id: req.params.id}, returning: true}
+)
+if(rowsAffected === 0) {
+  return res.status(422).json({message: `an error has occurred, category with id ${req.params.id} is not found in our records`});
+  
+} 
+res.status(200).json({message: `category with id ${req.params.id} has been successfully updated to ${req.body.category_name}!`})
+}catch(err){res.status(422).json({message: "Sorry, your request could not be processed because of the follwoing error - " + err});}
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-});
+router.delete('/:id', async (req, res) => {
 
-router.post('/seed', (req, res) => {
-  Category.bulkCreate([
-    {
-      category_name: 'Shirts',
-    },
-    {
-      category_name: 'Shorts',
-    },
-    {
-      category_name: 'Music',
-    },
-    {
-      category_name: 'Hats',
-    },
-    {
-      category_name: 'Shoes',
-    },
-  ])
-  .then(()=> {
-    res.status(200).json({message: "Categories table seeded"})
-  })
-})
+  try{
+    const deletedCategory = await Category.destroy({
+      where: {
+        id: req.params.id
+      }, returning: true
+    })
+    if(!deletedCategory) {
+      return res.status(422).json({message: `an error has occurred, category with id ${req.params.id} is not found in our records`})
+    }
+    res.status(200).json({message: `category with id ${req.params.id} has been deleted!`})
+  }
+  catch(err)
+  {res.status(422).json({message: "Sorry, your request could not be processed because of the follwoing error - " + err })}
+
+
+});
 
 module.exports = router;
